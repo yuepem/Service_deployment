fd10e3a82fec14808b8d09d1015829ef43de8900
x_yue <103266340+yuepem@users.noreply.github.com>
2024-06-28 14:22:47 +0200

    initialise the initail work branch

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <main>
-    <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-    <h1>Welcome to Next.js 13 </h1>
-   </main>
+    <main className="py-3 flex flex-col items-center  bg-slate-200">
+      <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} className="py-4"/>
+      <h1 className="py2">Welcome to Next.js 13 </h1>
+      <p className="text-balance text-sky-600 font-bold ">
+        This is the initial branch to work
+      </p>
+    </main>
   );
 }
