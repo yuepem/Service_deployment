360f3c8fdec3de592cd9290f090c355288864c49
x_yue <103266340+yuepem@users.noreply.github.com>
2024-06-28 15:56:16 +0200

    set up google and github auth providers/3

---
+++ server/auth.ts
@@ -0,0 +1,22 @@
+import NextAuth from "next-auth"
+import { DrizzleAdapter } from "@auth/drizzle-adapter"
+import { db } from "@/server"
+
+import google from "next-auth/providers/google"
+import github from "next-auth/providers/github"
+ 
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: DrizzleAdapter(db),
+  providers: [
+    // Add your Google provider
+    google({
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    // add more GitHub provider
+    github({
+        clientId: process.env.GITHUB_ID,
+        clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+})
---
+++ server/index.ts
@@ -0,0 +1,8 @@
+
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from 'drizzle-orm/neon-http';
+import * as schema from "@/server/schema"
+    
+const sql = neon(process.env.DATABASE_URL as string);
+
+export const db = drizzle(sql);
